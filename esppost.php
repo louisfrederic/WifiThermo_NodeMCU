27a1d5d96c2891f02f62edf361856d46e5e59457
louisfrederic <35235708+louisfrederic@users.noreply.github.com>
2018-01-08 20:39:53 +0100

    Add files via upload

--- esppost.php
+++ esppost.php
@@ -1,6 +1,9 @@
 <?php
 //create a variable that will recieve the temperature
 $Temp = $_POST["temperatur"];
+$date = $date->format("d-M-Y  h:i:s  ");
+
+
 $Write="<p>Temperatur : " . $Temp . " °C </p>";
-file_put_contents('index.html',$Write);
+file_put_contents('sensor.html',$Write);
 ?>
